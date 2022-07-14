5e0178c3b07c8f600d3ff5344d90225172b860ab
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:29:51 +0530

    Create DAY_2_ALG_6.c

---
+++ DAY2/DAY_2_ALG_6.c
@@ -0,0 +1,7 @@
+6>ALGORITHM
+CELSIUS AND FARENHEIT
+1.user has taken the input as celsius and farenheit vice versa.
+2.using formula for farenheit=(1.8*celsius)+32 and celsius=(farenheit-32)*5/9;
+3.Get the output as farenheit and celsius.
+
+
