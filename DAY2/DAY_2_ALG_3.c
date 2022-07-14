854939bba17fb1e8c91493b8822b128961c0b49e
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:27:00 +0530

    Create DAY_2_ALG_3.c

---
+++ DAY2/DAY_2_ALG_3.c
@@ -0,0 +1,6 @@
+3>ALGORITHM
+NUMBER OF HANDSHAKES
+1.Taken the input as no.of students and scan the input.
+2.no.of handshakes should be count when they meet each other.
+3.so,the equqtion is n*(n-1).
+4.print the total no.of handshakers.
