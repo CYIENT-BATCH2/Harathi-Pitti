4b1c184dc9b56ec15fd54e8932fe992ca8fe338b
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:05:10 +0530

    Create DAY_1_ALG_2.c

---
+++ DAY1/DAY_1_ALG_2.c
@@ -0,0 +1,7 @@
+
+2>ALGORITHM
+VOWEL OR CONSONANT
+1.Taken as the user input as a character.
+2.check that the upper case and lower case of 'a','e','i','o','u' is vowel or consonants
+3.if lower case or upper case occurs means print as a vowels.
+4.otherwise consonants.
