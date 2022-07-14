a295c713c01deffe032c9fa2fab82f736daf9157
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:08:29 +0530

    Create DAY_1_ALG_5.c

---
+++ DAY1/DAY_1_ALG_5.c
@@ -0,0 +1,6 @@
+5>ALGORITHM
+BALLS ARE EVEN OR ODD
+1.According to requirements the data will be taken as user.
+2.check that condition if that number is bitwise and(&)with 1 is equality of 0.
+3.whether the above condition is satisfied means it will print as "even number".
+4.otherwise it will print as "odd number".
