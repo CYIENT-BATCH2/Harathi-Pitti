949ceba3a352d291cd738cd956726e471b6c7882
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:24:51 +0530

    Create DAY_2_ALG_1.c

---
+++ DAY2/DAY_2_ALG_1.c
@@ -0,0 +1,8 @@
+1>ALGORITHM
+SUM OF DIGITS
+1.Taken the input of a mobile number as user.
+2.we want separate each number so only we can add.
+3.we find remainder of each digit until the remainder became zero.
+4.we should add the remainder with sum until the condition became false.
+5.print the sum of value.
+
