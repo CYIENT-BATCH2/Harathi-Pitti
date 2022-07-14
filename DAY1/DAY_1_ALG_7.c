12b039fbb8e0ee86e1f904deb873a2b82899c55e
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:10:23 +0530

    Create DAY_1_ALG_7.c

---
+++ DAY1/DAY_1_ALG_7.c
@@ -0,0 +1,9 @@
+
+7>ALGORITHM
+PALINDROME OR NOT
+1.we are taking the variable as temp,number,rem and rev.
+2.first we have to store that number into temp variable.
+3.if the remainder is greater than zero only it will it enter into the condition.
+4.Like,rev=rev*10+rem and number is stored in rev.
+5.check if temp is equality of rev only it will print as palindrome.
+6.otherwise it will print as not palindrome .
