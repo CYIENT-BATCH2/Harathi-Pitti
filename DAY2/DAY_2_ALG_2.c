a3a2b12192df00c01056e74556fe3a836403938b
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:25:54 +0530

    Create DAY_2_ALG_2.c

---
+++ DAY2/DAY_2_ALG_2.c
@@ -0,0 +1,7 @@
+2>ALGORITHM
+CONVERT DIGIT INTO BINARY
+1.user has taken the input as digit.
+2.We have to set the position as 7 using goto label.
+3.use the logic as num>>pos&1.
+4.print the binary value.
+
