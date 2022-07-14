aa4b923a9c9957335e585c80bb55d9ce7df49da9
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:39:14 +0530

    Create DAY_3_PROB_8.c

---
+++ DAY3/DAY_3_PROB_8.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter the alphabet\n");
+    fflush(stdout);
+    scanf("%c",&ch);
+    ((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z'))?printf("%c is alphabet\n",ch):printf("%c is not alphabet\n",ch);
+}
+
