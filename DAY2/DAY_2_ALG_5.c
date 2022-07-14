dc7471f08f2806cb4d946f29c068ac88a5d65407
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:28:40 +0530

    Create DAY_2_ALG_5.c

---
+++ DAY2/DAY_2_ALG_5.c
@@ -0,0 +1,7 @@
+5>ALGORITHM
+ENCODED AND DECODED
+1.enter the number as input.
+2.if that character is between from '0' to '9' only the condition should be executed.
+3.if the above condition is true that ascii value should be added with 16.
+4.print the decoded as character.
+
