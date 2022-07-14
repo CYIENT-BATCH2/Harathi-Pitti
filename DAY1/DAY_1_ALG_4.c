22ebe04fc5263a1fa4e523e02479476a19b4b9aa
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:07:17 +0530

    Create DAY_1_ALG_4.c

---
+++ DAY1/DAY_1_ALG_4.c
@@ -0,0 +1,9 @@
+4>ALGORITHM
+REVERSE THE NUMBER
+1.Taken the user input as number.
+2.using goto label we are giving the condition.
+3.if the remainder greater than zero only it will go into loop.
+4.using that equation like,rev=rev*10+rem we are finding and storing it.
+5.check number!=0 otherwise it will exit from the condition.
+6.print the reverse number.
+
