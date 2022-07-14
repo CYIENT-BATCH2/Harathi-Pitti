954d8ab969b544e2047f165284903373cec785fe
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:06:18 +0530

    Create DAY_1_ALG_3.c

---
+++ DAY1/DAY_1_ALG_3.c
@@ -0,0 +1,7 @@
+3>ALGORITHM
+AREA OF CIRCULAR GARDEN
+1.we are taken the input datatype as float.
+2.Enter the radius as user input.
+3.we know that formula to find area,so using 3.14*r*r we can find.
+4.print the area in float format.
+
