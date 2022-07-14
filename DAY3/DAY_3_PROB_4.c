28fadb03c1330f3cef3933df2dcacb658e483cbf
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:35:53 +0530

    Create DAY_3_PROB_4.c

---
+++ DAY3/DAY_3_PROB_4.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+    int a=204;
+    long int pos=7;
+    printf("a=%x\n",a);
+    label:
+    if(pos>=0)
+    {
+        printf("%d",(a>>pos)&1);
+        pos--;
+        goto label;
+    }
+}
+
