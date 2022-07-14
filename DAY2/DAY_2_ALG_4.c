d4185069d6d520373531fdc2a0bf5ab2f949e6b7
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:27:51 +0530

    Create DAY_2_ALG_4.c

---
+++ DAY2/DAY_2_ALG_4.c
@@ -0,0 +1,7 @@
+
+4>ALGORITHM
+NUMBER OF LOTTERY TICKETS
+1.Enter the lottery tickets as user.
+2.using goto label that number is nor equal to zero.
+3.if the rem is equqlity of 3 the count should be increased.
+4.The count is greater than zero and print the count as the handshakes.
