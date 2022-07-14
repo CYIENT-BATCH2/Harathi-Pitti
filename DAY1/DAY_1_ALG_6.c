ebc60dbf2c3532753997d993ef7bf6f95e906a0f
HarathiPitti <108974031+HarathiPitti@users.noreply.github.com>
2022-07-14 14:09:21 +0530

    Create DAY_1_ALG_6.c

---
+++ DAY1/DAY_1_ALG_6.c
@@ -0,0 +1,8 @@
+6>ALGORITHM
+ASCENDING ORDER OF STUDENTS ROLL NUMBER
+1.we are taken the input of student roll as a user.
+2.using goto label condition whether the 1st roll no. is greater than the 2nd roll no. or not.
+3.if it is true means we taken as another variable like temp to store the value.
+4.1st roll no. is stored in temp and 2nd roll no. is stored in 1st roll no. and again temp value is stored in 2nd roll no.
+5.likewise if that condition is true means it will following like that until the condition get value.
+6.finely,it will print the otherwise student roll number.
